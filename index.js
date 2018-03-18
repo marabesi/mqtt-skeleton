b39d8e46a37e1b9801191e8346992c23a5490bc8
marabesi <matheus.marabesi@gmail.com>
2018-03-18 18:54:41 -0300

    added express to use heroku

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 require('dotenv').config();
 
+const express = require('express');
+const path = require('path');
+
 const host     = process.env.MQTT_SERVER;
 const user     = process.env.MQTT_USER;
 const password = process.env.MQTT_PASSWORD;
 const port     = process.env.MQTT_PORT;
 const topic    = process.env.MQTT_TOPIC;
 
+const PORT = process.env.PORT || 5000;
+
 const mqtt = require('mqtt');
 const client = mqtt.connect('mqtt://' + user + ':' + password + '@' + host + ':' + port);
+
+express()
+  .use(express.static(path.join(__dirname, 'hivemq-mqtt-web-client')))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 client.on('connect', function () {
     console.log('Connected');
